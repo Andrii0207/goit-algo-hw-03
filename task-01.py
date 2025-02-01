fb2ba5e4de0f8a0cfd9cf4cae07bc8ac6f6b72c8
Andrii Popov <Popov.Andrey80@gmail.com>
2025-02-01 14:36:24 +0200

    add exceptions

--- task-01.py
+++ task-01.py
@@ -7,24 +7,31 @@ def backup_data(initial_path, backup_path="dist", indent=0, prefix=""):
 
     data = Path(initial_path)
 
-    if data.is_dir():
-        print(" " * indent + prefix + Fore.BLUE +
-              f"{data}/" + Style.RESET_ALL)
-        children = sorted(
-            data.iterdir(), key=lambda x: (x.is_file(), x.name))
+    try:
+        if data.is_dir():
+            print(" " * indent + prefix + Fore.BLUE +
+                  f"{data}/" + Style.RESET_ALL)
+            children = sorted(
+                data.iterdir(), key=lambda x: (x.is_file(), x.name))
 
-        for index, child in enumerate(children):
-            is_last = index == len(children) - 1
-            new_prefix = "└── " if is_last else "├── "
-            backup_data(child, backup_path, indent + 4, new_prefix)
+            for index, child in enumerate(children):
+                is_last = index == len(children) - 1
+                new_prefix = "└── " if is_last else "├── "
+                backup_data(child, backup_path, indent + 4, new_prefix)
 
-    else:
-        print(" " * indent + prefix + Fore.YELLOW +
-              f"{data.name}" + Style.RESET_ALL)
-        ext = data.suffix
-        new_path = Path(backup_path) / ext
-        new_path.mkdir(exist_ok=True, parents=True)
-        shutil.copyfile(data, new_path / data.name)
+        else:
+            print(" " * indent + prefix + Fore.YELLOW +
+                  f"{data.name}" + Style.RESET_ALL)
+            ext = data.suffix
+            new_path = Path(backup_path) / ext
+            new_path.mkdir(exist_ok=True, parents=True)
+            shutil.copyfile(data, new_path / data.name)
+
+    except FileNotFoundError:
+        print(f"Помилка: файл або директорія \'{data}\' не знайдено")
+
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
 
 
 if __name__ == "__main__":
